68a5abf3af262b52aebbbbe06b5e855415158dd8
ahankuo <ahan.net@gmail.com>
2016-09-29 09:57:24 +0800

    Add files via upload

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -37514,7 +37514,7 @@ angular.module('mm.core')
 
 .constant('mmCoreConfigConstants', {
     "app_id" : "tw.edu.ksu.moodle",
-    "versioncode" : "2014",
+    "versioncode" : "20160901",
     "versionname" : "3.1.2",
     "cache_expiration_time" : 300000,
     "default_lang" : "zh-tw",
